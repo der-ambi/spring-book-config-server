a52a7afb3ef81d9e30ff6e9a5a17c4d5e18829e6
Christian Ambach <christian.ambach@codecentric.de>
2024-08-13 12:45:44 +0200

    enable config server

--- src/main/kotlin/de/bashburg/springbook/configservice/ConfigServiceApplication.kt
+++ src/main/kotlin/de/bashburg/springbook/configservice/ConfigServiceApplication.kt
@@ -2,8 +2,10 @@ package de.bashburg.springbook.configservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class ConfigServiceApplication
 
 fun main(args: Array<String>) {
