c80418057b7af1a38290b8f00edd1bd54a149f6b
Christian Ambach <christian.ambach@codecentric.de>
2024-08-13 12:05:26 +0200

    initial after generation

---
+++ src/main/kotlin/de/bashburg/springbook/configservice/ConfigServiceApplication.kt
@@ -0,0 +1,11 @@
+package de.bashburg.springbook.configservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigServiceApplication>(*args)
+}
---
+++ src/test/kotlin/de/bashburg/springbook/configservice/ConfigServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.bashburg.springbook.configservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
